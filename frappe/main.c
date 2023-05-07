d2476a1d3da8d1358bfda52f2a7eca3f3198ca35
Jason Conway <jpc@jasonconway.dev>
2023-05-07 15:59:56 -0500

    Initial Commit

---
+++ frappe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  frappe
+//
+//  Created by Jason Conway on 5/7/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
